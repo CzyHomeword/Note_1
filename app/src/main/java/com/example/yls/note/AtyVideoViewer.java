a1b3bff259c07de97c834d311dbe81c93b653e3e
CzyHomeword <953678433@qq.com>
2017-05-15 08:53:43 +0800

    记事本

---
+++ app/src/main/java/com/example/yls/note/AtyVideoViewer.java
@@ -0,0 +1,29 @@
+package com.example.yls.note;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+/**
+ * Created by yls on 2017/5/11.
+ */
+
+public class AtyVideoViewer  extends Activity {
+    private VideoView vv;
+    public static final String EXTRA_PATH = "path";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        vv = new VideoView(this);
+        vv.setMediaController(new MediaController(this));
+        setContentView(vv);
+        String path = getIntent().getStringExtra(EXTRA_PATH);
+        if (path != null) {
+            vv.setVideoPath(path);
+        } else {
+            finish();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/yls/note/MediaAdapter.java
@@ -0,0 +1,9 @@
+package com.example.yls.note;
+
+/**
+ * Created by yls on 2017/5/11.
+ */
+public class MediaAdapter {
+    public MediaAdapter(AtyEditNote atyEditNote) {
+    }
+}
